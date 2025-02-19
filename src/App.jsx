e1711025926ad6bc2a48ac8a1864fc16f7b0c06a
Akash Parmar <ak.parmar1442000@gmail.com>
2025-02-19 21:29:29 +0530

    installed react-router-dom dependency and set it up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { FloatingDock } from "./Components/FloatingDock";
+import { AnimatePresence } from "motion/react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import UiLayout from "./Components/UiLayout";
 
 function App() {
   const [count, setCount] = useState(0);
+  const location = useLocation();
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline"> Hello world! </h1>
-    </>
+    <main className="main-container">
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<UiLayout />} />
+        </Routes>
+      </AnimatePresence>
+      <section className="fixed z-50 min-w-fit right-8 sm:right-auto sm:left-1/2 sm:-translate-x-1/2 bottom-8 sm:bottom-10">
+        <FloatingDock />
+      </section>
+    </main>
   );
 }
 
