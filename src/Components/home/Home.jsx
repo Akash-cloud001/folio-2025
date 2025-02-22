c49c089c35b1b2a1de052b49486d0f6383d85eae
Akash Parmar <ak.parmar1442000@gmail.com>
2025-02-22 23:42:42 +0530

    home page desktop ui done

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,35 +1,54 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import HeroRightScene from "./HeroRightScene";
-import { Headphone } from "./Headphone";
-import { Center, OrbitControls } from "@react-three/drei";
+import {motion} from 'motion/react'
+import Orb from '../Orb/Orb'
+import { spring } from "motion";
+
 const Home = () => {
+  const [mouseY, setMouseY] = useState('0');
+
+  const handleMouseMove = (e)=>{
+    setMouseY(String((e.clientY / window.innerHeight * 180 - 90) * 0.04)+'deg');
+    console.log(e.clientY / window.innerHeight * 180)
+  }
+  const rotation = (mouseY / window.innerHeight) * 180 - 90;
   return (
-    <section className="home-container h-screen w-full flex items-center justify-center">
-      <article className="hero-left w-3/4 h-full ff-betatron text-[150px] flex items-start justify-center pl-6 flex-col">
-        <div className="flex items-center justify-start">
-          <div className="headPhone h-[150px] w-[150px]">
-            <Canvas
-              gl={{
-                powerPreference: "high-performance",
-                alpha: false,
-                antialias: true,
-              }}
-            >
-            <color attach="background" args={["#0a0a0a"]} />
-              <Suspense fallback={null}>
-                <Center>
-                  <Headphone scale={0.8} />
-                </Center>
-                <ambientLight intensity={2} color={'white'}/> 
-              </Suspense>
-            </Canvas>
-          </div>
+    <section onMouseMove={handleMouseMove} className="home-container h-screen w-full flex items-center justify-center">
+      <article className="hero-left w-3/4 h-full ff-betatron text-[150px] flex items-start justify-center pl-6 flex-col relative z-[1] select-none">
+        <div className="flex items-center justify-start text-light">
+          <motion.div className="headPhone h-[150px] w-[150px]" style={{rotate: mouseY, transformOrigin:'left center'}} >
+            <img
+              src="/svgs/headphone.svg"
+              alt="headphone"
+              className="-rotate-90 h-full w-full"
+            />
+          </motion.div>
           <span>REATIVE</span>
         </div>
-        <p className="-mt-20 text-stroke flex items-center justify-start">
-          <span>DEVEL</span> <span>PER</span>
-        </p>
+        <div className="-mt-20 text-stroke flex items-center justify-start">
+          <span>DEVEL</span>
+          <div className="h-[150px] w-[150px] relative">
+            <Orb
+              hoverIntensity={0.5}
+              rotateOnHover={true}
+              hue={0}
+              forceHoverState={false}
+            />
+          </div>
+          <span>PER</span>
+        </div>
+        <div className="w-4/5 ff-gs-regular text-base flex items-start justify-start text-light opacity-70 pl-6 relative top-16">
+          <div className="relative">
+            <div className="absolute -left-5 top-[10px] flex gap-1">
+              <div className="h-1 w-1 rounded-full bg-light-900"></div>
+              <div className="h-1 w-1 rounded-full bg-light-900"></div>
+            </div>
+            <p className="tracking-wide">A Frontend Developer</p>
+            <p className="tracking-wide">specializing in Crafting Interactive</p>
+            <p className="tracking-wide">and Immersive Digital Experiences.</p>
+          </div>
+        </div>
       </article>
       <aside id="hero-canvas" className="hero-right w-1/4 h-full ">
         <Canvas
