3aee9dbd884dbc25a14b509fc8793e6b4fba6f60
Akash Parmar <ak.parmar1442000@gmail.com>
2025-02-25 23:07:23 +0530

    working on responsiveness for home function component file

--- src/Components/home/HeroRightScene.jsx
+++ src/Components/home/HeroRightScene.jsx
@@ -1,52 +1,71 @@
-import React, { useRef } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { useLoader } from "@react-three/fiber";
 import { Hologram } from "./Hologram";
 import { Center, OrbitControls, useHelper } from "@react-three/drei";
 import { useControls } from "leva";
-import * as THREE from 'three'
-
+import * as THREE from "three";
 
 const HeroRightScene = () => {
-  
-  // const {x,y,z, rx, ry, rz,scale} = useControls('heroleft', {
-  //     x: {value: 1.60, min: -10, max: 10, step: 0.01},
-  //     y: {value: 0.15, min: -10, max: 10, step: 0.01},
-  //     z: {value: 0, min: -10, max: 10, step: 0.01},
-  //     rx: {value: 0, min: -Math.PI, max: Math.PI, step: 0.01},
-  //     ry: {value: 0, min: -Math.PI, max: Math.PI, step: 0.01},
-  //     rz: {value: 1.59, min: -Math.PI, max: Math.PI, step: 0.01},
-  //     scale: {value: 13.1, min: 1, max: 20}
-  // })
+  const [resize, setResize] = useState(10);
+
+  useLayoutEffect(() => {
+    function handleResize() {
+      const screen = window.innerWidth;
+
+      if (screen >= 1280) {
+        setResize(13.1);
+      } else if (screen >= 1028) {
+        setResize(12.0);
+      } else if (screen >= 768) {
+        setResize(10);
+      } else {
+        setResize(10);
+      }
+    }
+
+    handleResize(); // Initial call to set the correct size
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const { x, y, z } = useControls("heroleft", {
+    x: { value: 0, min: -Math.PI, max: Math.PI, step: 0.01 },
+    y: { value: 0, min: -Math.PI, max: Math.PI, step: 0.01 },
+    z: { value: 1.59, min: -Math.PI, max: Math.PI, step: 0.01 },
+    scale: { value: 13.1, min: 1, max: 20 },
+  });
   // const {x,y,z, power} = useControls('pointLight', {
   //     x: {value: 0.10, min: -10, max: 10, step: 0.01},
   //     y: {value: -0.30, min: -10, max: 10, step: 0.01},
   //     z: {value: -1.60, min: -10, max: 10, step: 0.01},
   //     power: {value: 100, min: 0, max: 500}
   // })
-  const ref = useRef()
+  const ref = useRef();
 
   // useHelper(ref, THREE.PointLightHelper, 0.5, '#00f')
   return (
     <>
       <hemisphereLight skyColor={"#fff"} groundColor={"#fff"} intensity={4} />
-        <pointLight
-          ref={ref}
-          position={[0.10, -0.30, -1.60]} // Centered in the model
-          color={"#00f"} // Blue light
-          power={500} // Higher power for better intensity
-          distance={10} // Adjust range
-          decay={10} // Light fading
-        />
+      <pointLight
+        ref={ref}
+        position={[0.1, -0.3, -1.6]} // Centered in the model
+        color={"#00f"} // Blue light
+        power={500} // Higher power for better intensity
+        distance={10} // Adjust range
+        decay={10} // Light fading
+      />
       <Center>
         {/* <Hologram scale={scale} position={[x,y,z]} rotation={[rx, ry, rz]}  /> */}
         <Hologram
-          scale={13.1}
+          scale={resize}
           position={[1.6, 0.15, 0]}
-          rotation={[0, 0, 1.59]}
+          rotation={[x, y, z]}
           delay={0}
         />
       </Center>
-
     </>
   );
 };
--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -14,10 +14,10 @@ const Home = () => {
   }
   const rotation = (mouseY / window.innerHeight) * 180 - 90;
   return (
-    <section onMouseMove={handleMouseMove} className="home-container h-screen w-full flex items-center justify-center">
-      <article className="hero-left w-3/4 h-full ff-betatron text-[150px] flex items-start justify-center pl-6 flex-col relative z-[1] select-none">
+    <section onMouseMove={handleMouseMove} className="home-container relative h-screen md:h-screen w-full flex flex-col-reverse md:flex-row items-start justify-center md:items-center md:justify-center">
+      <article className="hero-left w-3/4 h-full ff-betatron text-7xl xl:text-[150px] flex items-start justify-center pl-6 flex-col relative z-[1] select-none -top-48 md:top-auto">
         <div className="flex items-center justify-start text-light">
-          <motion.div className="headPhone h-[150px] w-[150px]" style={{rotate: mouseY, transformOrigin:'left center'}} >
+          <motion.div className="headPhone h-[76px] w-[76px] xl:h-[150px] xl:w-[150px]" style={{rotate: mouseY, transformOrigin:'left center'}} >
             <img
               src="/svgs/headphone.svg"
               alt="headphone"
@@ -26,9 +26,9 @@ const Home = () => {
           </motion.div>
           <span>REATIVE</span>
         </div>
-        <div className="-mt-20 text-stroke flex items-center justify-start">
+        <div className="mt-0 text-stroke flex items-center justify-start">
           <span>DEVEL</span>
-          <div className="h-[150px] w-[150px] relative">
+          <div className="h-[76px] w-[76px] xl:h-[150px] xl:w-[150px] relative">
             <Orb
               hoverIntensity={0.5}
               rotateOnHover={true}
@@ -38,19 +38,19 @@ const Home = () => {
           </div>
           <span>PER</span>
         </div>
-        <div className="w-4/5 ff-gs-regular text-base flex items-start justify-start text-light opacity-70 pl-6 relative top-16">
+        <div className="w-4/5 ff-gs-regular text-base flex items-start justify-start text-light opacity-70 pl-6 relative top-8 md:top-16">
           <div className="relative">
             <div className="absolute -left-5 top-[10px] flex gap-1">
               <div className="h-1 w-1 rounded-full bg-light-900"></div>
               <div className="h-1 w-1 rounded-full bg-light-900"></div>
             </div>
-            <p className="tracking-wide">A Frontend Developer</p>
-            <p className="tracking-wide">specializing in Crafting Interactive</p>
-            <p className="tracking-wide">and Immersive Digital Experiences.</p>
+            <p className="tracking-wide max-w-xl">A Frontend Developer specializing in Crafting Interactive and Immersive Digital Experiences.</p>
+            {/* <p className="tracking-wide">specializing in Crafting Interactive</p> */}
+            {/* <p className="tracking-wide">and Immersive Digital Experiences.</p> */}
           </div>
         </div>
       </article>
-      <aside id="hero-canvas" className="hero-right w-1/4 h-full ">
+      <aside id="hero-canvas" className="hero-right relative top-10 md:top-auto w-[100%] md:w-[40%] lg:w-[30%]  xl:w-1/4 h-[100vh] md:h-full ">
         <Canvas
           gl={{
             powerPreference: "high-performance",
@@ -59,6 +59,7 @@ const Home = () => {
             // stencil: true,
             // depth: true,
           }}
+          className="w-full"
         >
           <color attach="background" args={["#0a0a0a"]} />
           <Suspense fallback={null}>
