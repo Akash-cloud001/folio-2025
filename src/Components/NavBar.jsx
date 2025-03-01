8a5d49f646df6b52a749a70f55e42088d323fb3b
Akash <ak.parmar1442000@gmail.com>
2025-03-01 21:07:14 +0530

    added navbar background effect

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className='fixed top-0 left-0 w-full z-50 pt-4 md:pt-8'>
-        <img src="/svgs/akash.svg" alt="portfolio logo" className='block mx-auto'/>
+    <nav className='navbar-container fixed top-0 left-0 w-full z-50 pt-4 md:pt-8'>
+        <img src="/svgs/akash.svg" alt="portfolio logo" className='block mx-auto relative z-10'/>
     </nav>
   )
 }
