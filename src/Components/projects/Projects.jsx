c94f001fead53e9c36b294369da48fc8e424248b
Akash <ak.parmar1442000@gmail.com>
2025-03-05 19:52:07 +0530

    created project card

--- src/Components/projects/Projects.jsx
+++ src/Components/projects/Projects.jsx
@@ -9,7 +9,7 @@ const projectsData=[
     imgUrl: '/images/pro-1.jpeg',
     videoUrl:'',
     backgroundFrame: 'red',
-    tech: ['react', 'tailwind', "api's"],
+    tech: ['HTML','CSS','react', 'tailwind', "api's"],
     desc: "AniPedia is a web application designed for anime enthusiasts, offering up-to-date information on currently airing anime and top performers. Leveraging the Jikan API, it provides detailed insights into various anime series. Users can search for specific anime, watch trailers via YouTube, and find platforms where they can stream their chosen series."
   },
   {
@@ -42,7 +42,7 @@ const Projects = () => {
               <p className='uppercase ff-betatron text-nowrap text-stroke absolute z-0 left-1 top-1'>MY WORK</p>
             </div>
           </div>
-          <motion.section className='project-scroller h-screen w-full bg-transparent absolute z-[1]'>
+          <motion.section className='project-scroller h-screen w-full bg-transparent absolute z-[1] bg-red-500'>
             <ProjectCard name={projectsData[0].name} url={projectsData[0].imgUrl} tech={projectsData[0].tech} idx={1}/>
           </motion.section>
         </section>
@@ -57,28 +57,36 @@ export default Projects
 const ProjectCard = ({name, url, tech, idx})=>{
   return(
     <motion.aside className='p-4'>
-      <figure className='project-card p-[10px] w-[300px] h-[auto] border border-[rgba(251,250,243,0.2)] relative'>
+      <figure className='project-card p-[10px] w-[320px] h-[auto] border border-[rgba(251,250,243,0.2)] relative'>
         <img src="/images/cross.png" alt="" className='absolute -top-[5px] -left-[5px]' /> 
         <img src="/images/cross.png" alt="" className='absolute -top-[5px] -right-[5px]' /> 
         <img src="/images/cross.png" alt="" className='absolute -bottom-[5px] -left-[5px]' /> 
         <img src="/images/cross.png" alt="" className='absolute -bottom-[5px] -right-[5px]' /> 
-        <img src={url} alt="pro-image" className='w-full h-[150px] object-cover object-top'/>
-        <div className='bg-[rgba(251,250,243,0.03)] h-[200px] w-full'>
-          <div className='relative text-4xl w-max h-fit mx-auto pt-4'>
-                <p className='uppercase ff-betatron text-primary z-[1]'>{name}</p>
+        <img src={url} alt="pro-image" className='w-full h-[160px] object-cover object-top'/>
+        <div className='bg-transparent h-[200px] w-full'>
+          <div className='relative text-4xl w-max h-fit mx-auto pt-4 flex gap-1 text-primary'>
+                <p className='uppercase ff-betatron  z-[1]'>{name}</p>
                 <p className='uppercase ff-betatron text-nowrap text-stroke-red absolute z-0 left-1 top-3'>{name}</p>
+                <div className='relative'>
+                <sub className='relative -top-5  text-base'>[0{idx}]</sub>
+                </div>
           </div>
         </div>
         <Gravity gravity={{ x: 0, y: 1 }} className="w-full h-full">
-        <MatterBody
-          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-          x="30%"
-          y="10%"
-        >
-          <div className="text-xl sm:text-2xl md:text-3xl bg-[#0015ff] text-white rounded-full hover:cursor-pointer px-8 py-4">
-            react
-          </div>
-        </MatterBody>
+          {tech.map((item, idx)=>(
+              <MatterBody
+              key={idx}
+                matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
+                x= {String((idx+4) * 10)+'%'}
+                // y= {String((idx+1) * 10)+'%'}
+                y="10%"
+              >
+                <div className=" font-extralight text-xs uppercase bg-transparent border border-[#FBFAF3] rounded-full hover:cursor-pointer px-3 py-1">
+                  {item}
+                </div>
+              </MatterBody>
+
+          ))}
         </Gravity>
       </figure>
     </motion.aside>
