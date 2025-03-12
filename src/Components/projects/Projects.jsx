4a7641cc57cc2873c93a1bb2ac6fc6406ff7bc02
Akash <ak.parmar1442000@gmail.com>
2025-03-12 21:42:29 +0530

    responsiveness done

--- src/Components/projects/Projects.jsx
+++ src/Components/projects/Projects.jsx
@@ -54,8 +54,8 @@ const Projects = () => {
           {/* </motion.section> */}
         </section>
         <ProjectCard name={projectsData[2].name} url={projectsData[2].url} imgUrl={projectsData[2].imgUrl} tech={projectsData[2].tech} idx={1} className={'sticky top-[20%] left-[0%] -translate-x-1/2 w-max'}/>
-        <ProjectCard name={projectsData[1].name} url={projectsData[1].url} imgUrl={projectsData[1].imgUrl} tech={projectsData[1].tech} idx={2} className={'sticky top-[20%] left-[38%] -translate-x-1/2 w-max'}/>
-        <ProjectCard name={projectsData[0].name} url={projectsData[0].url} imgUrl={projectsData[0].imgUrl} tech={projectsData[0].tech} idx={3} className={'sticky top-[20%] left-[80%] -translate-x-1/2 w-max'}/>
+        <ProjectCard name={projectsData[1].name} url={projectsData[1].url} imgUrl={projectsData[1].imgUrl} tech={projectsData[1].tech} idx={2} className={'sticky top-[20%] left-[25%] lg:left-[34%] xl:left-[38%] -translate-x-1/2 w-max'}/>
+        <ProjectCard name={projectsData[0].name} url={projectsData[0].url} imgUrl={projectsData[0].imgUrl} tech={projectsData[0].tech} idx={3} className={'sticky top-[20%] left-[75%] lg:left-[100%] -translate-x-1/2 w-max'}/>
         {/* <ProjectCard name={projectsData[2].name} url={projectsData[2].imgUrl} tech={projectsData[2].tech} idx={1} className={'sticky top-[20%] '}/> */}
       </section>
     </section>
