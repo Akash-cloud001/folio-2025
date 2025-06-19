4e0117c13d0d3326487470e629494a356c66a1c4
Akash <ak.parmar1442000@gmail.com>
2025-06-20 03:04:39 +0530

    Update transition durations in transition.jsx to 3 seconds for smoother animations on entry and exit effects.

--- src/transition.jsx
+++ src/transition.jsx
@@ -34,14 +34,14 @@ const transition = (OgComp) => {
         initial={{scaleY:0}}
         animate={{scaleY:0}}
         exit={{scaleY: 1}}
-        transition={{duration: 2, ease:[0.22, 1, 0.36, 1]}}
+        transition={{duration: 3, ease:[0.22, 1, 0.36, 1]}}
       />
       <motion.div
         className='slide-out'
         initial={{scaleY:1}}
         animate={{scaleY:0}}
         exit={{scaleY: 0}}
-        transition={{duration: 2, ease:[0.22, 1, 0.36, 1]}}
+        transition={{duration: 3, ease:[0.22, 1, 0.36, 1]}}
       />
 
     </>
